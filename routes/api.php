62f9b0541a921027fe126b771f889d8e0ebcab6e
andrew <monkeyzealer@gmail.com>
2017-04-14 14:50:38 -0400

    worked on sign in and sign up backend

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post("signUp", "AuthController@signUp");
+Route::post("signIn", "AuthController@signIn");
 Route::post("updateArticle/{id}", "MainController@updateArticle");
 Route::post("storeArticle", "MainController@storeArticle");
 Route::post("destroyArticle/{id}", "MainController@destroyArticle");
